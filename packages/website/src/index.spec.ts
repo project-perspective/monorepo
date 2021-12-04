1c1776409180335a067decd1a5203c081aa8b039
Swapnil Singh <mail@swapnilsingh.me>
2021-12-04 19:35:07 +0530

    Adds github link

--- packages/website/src/index.spec.ts
+++ packages/website/src/index.spec.ts
@@ -1,16 +1,24 @@
 import index from './index'
 
 describe('index', () => {
-	it('should contain heading', () => {
-		const update = new index().render();
-		// update(host, div)
-		const heading = update.querySelector('[data-test-id="heading"]');
-		expect(heading).not.to.equal(null)
+	it('should contain logo', () => {
+		const element = new index().render();
+		expect(element.querySelector('[data-test-id="logo"]')).not.to.equal(null)
 	})
+	
+	it('should contain heading', () => {
+		const element = new index().render();
+		expect(element.querySelector('[data-test-id="heading"]')).not.to.equal(null)
+	})
+
 	it('should contain description', () => {
-		const update = new index().render();
-		// update(host, div)
-		const heading = update.querySelector('[data-test-id="description"]');
-		expect(heading).not.to.equal(null)
+		const element = new index().render();
+		expect(element.querySelector('[data-test-id="description"]')).not.to.equal(null)
+	})
+
+	it('should have correct github link', () => {
+		const element = new index().render();
+		const link = element.querySelector('[data-test-id="github-link"]');
+		expect(link.getAttribute('href')).to.equal("https://github.com/project-perspective")
 	})
 })
--- packages/website/src/index.ts
+++ packages/website/src/index.ts
@@ -2,10 +2,20 @@ import { html, define } from 'hybrids';
 import globals from './index.css'
 
 const render = () => html`
-	<main class="w-screen font-sans background bg-indigo-900 h-screen flex flex-col justify-center">
-		<h1 data-test-id="heading" class="italic text-white text-3xl self-center flex-grow-0">Different perspectives lead to better knowledge</h1>
-		<p data-test-id="description" class="text-white text-lg self-center my-8 flex-grow-0">Perspective is a decentralised graph wiki without censorship. <br />Anyone can add and learn from each others perspectives</p>
-	</main>
+	<div class="w-screen font-sans py-12 background bg-indigo-900 h-screen flex justify-center items-center">
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+			<div class="lg:text-center">
+				<h2 data-test-id="logo" class="text-base text-indigo-300 font-semibold tracking-wide uppercase">Project Perspective</h2>
+				<p data-test-id="heading" class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+					Different perspectives lead to a better understanding
+				</p>
+				<p data-test-id="description" class="mt-4 max-w-2xl text-xl text-gray-400 lg:mx-auto">
+					Perspective.wiki is a decentralised graph wiki without censorship. <br />Anyone can add and learn from each others perspectives
+				</p>
+				<a data-test-id="github-link" class="text-white my-8 inline-block" href="https://github.com/project-perspective">github</a>
+			</div>
+		</div>
+	</div>
 `.style(globals)
 
 const HomePage = define<HTMLElement>('home-page', { render })
